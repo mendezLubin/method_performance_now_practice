1e8c7879eb5cfb4651c1f3285205c20a21597f54
Lubin Mendez <mendezlubin@yahoo.com>
2025-05-31 11:20:46 -0700

    Display count with .textContent and add pause functionality using clearInterval

--- js/code.js
+++ js/code.js
@@ -1,18 +1,33 @@
-let playButton = document.querySelector(".playBtn");
-let pauseButton = document.querySelector(".pauseBtn");
+// setInterval(func, time) tells JavaScript to call a function repeatedly at a specified interval, for example every 1000ms (1 second).
+
+// When you do this, it returns a number (an ID). So besides calling the function, we store the generated ID in a variable, e.g., intervalId = setInterval(funcCheckTime, 1000);. This ID acts as the name or reference to the timer that was created.
+
+// This ID is necessary to stop it using clearInterval(id).
+// setInterval() and clearInterval() work together. clearInterval stops the interval, but it needs the generated ID to know which one to stop.
+
+let selPlayButton = document.querySelector(".playBtn");
+let selPauseButton = document.querySelector(".pauseBtn");
+let selCount= document.querySelector(".count");
 
 let timeElapsed= null;
+let intervalId= null;
 
 // Add event listener to the play button
-playButton.addEventListener("click", funcCallEverySecond);
+selPlayButton.addEventListener("click", funcCallEverySecond);
 
 function funcCallEverySecond() {
   timeElapsed= performance.now();
-  setInterval(funcCheckTime, 1000);
+  intervalId= setInterval(funcCheckTime, 1000);
 }
 
 function funcCheckTime() {
   const now= performance.now();
-  //  console.log("Time elapsed is: " + timeElapsed + " ms");
-  console.log(now - timeElapsed);
+  selCount.textContent= now - timeElapsed ;
+}
+
+selPauseButton.addEventListener("click", pauseTimer);
+
+function pauseTimer() {
+  clearInterval(intervalId);
+  
 }
