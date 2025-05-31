35b1e32d0d75e803a240b5ef388b19d18fa116ef
Lubin Mendez <mendezlubin@yahoo.com>
2025-05-30 22:57:30 -0700

    Calculate real elapsed time every second subtracting now- timeUntilButtonPress

--- js/code.js
+++ js/code.js
@@ -1,14 +1,18 @@
 let playButton = document.querySelector(".playBtn");
 let pauseButton = document.querySelector(".pauseBtn");
 
+let timeElapsed= null;
+
 // Add event listener to the play button
 playButton.addEventListener("click", funcCallEverySecond);
 
 function funcCallEverySecond() {
+  timeElapsed= performance.now();
   setInterval(funcCheckTime, 1000);
 }
 
 function funcCheckTime() {
-  const now = performance.now();
-  console.log(now);
+  const now= performance.now();
+  //  console.log("Time elapsed is: " + timeElapsed + " ms");
+  console.log(now - timeElapsed);
 }
